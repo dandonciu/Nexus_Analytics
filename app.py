7ed9f59b204a42cca16e4ef3c946813311b99f07
dandonciu <115440690+dandonciu@users.noreply.github.com>
2026-06-11 13:11:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ if st.session_state.active_tab == "📊 Dashboard Executiv":
     if selected_category == "Toate categoriile":
         st.markdown("""
         <div style="color: #475569; background-color: #f1f5f9; border-left: 4px solid #EBA11F; padding: 6px 12px; border-radius: 6px; font-size: 12px; margin-bottom: 15px; font-family: sans-serif;">
-        ⚠️ Notă de simulare: Datele utilizate sînt incomplete și doar parțial corecte. După finalizarea bazelor de date și sincronizarea cu WMS și SmartBill acest inconvenient dispare.
+        ⚠️ <b>Notă de simulare:<b> Datele utilizate sînt incomplete și doar parțial corecte. După finalizarea bazelor de date și sincronizarea cu WMS și SmartBill acest inconvenient dispare.
         <br><br>  
         ℹ️ <b>Precizare:</b> Toate cantitățile de tip Palet au fost convertite automat în Cutii/Bucăți la nivel de memorie (tabel dB) pentru a oferi grafice consolidate. Analiza valorică (financiară), în curs de implementare, va fi posibilă după completarea bazelor de date. Pentru a evita cumularea cantitativă a unor unități de măsură diferite, se recomandă selectarea unei categorii specifice din meniul din stânga.
         </div>
